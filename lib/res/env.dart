63da5cbfba06827b91075ea9dc0505e6a4d91e74
Andrea Gonzales <AndreaGon@users.noreply.github.com>
2023-12-01 14:45:58 +0800

    Backup

--- lib/res/env.dart
+++ lib/res/env.dart
@@ -1,2 +1,2 @@
 const GOOGLE_API_KEY = "AIzaSyAPxN6UrJeSCo6uDciO5EK--QCxo2cvros";
-const API_URL = "http://b600-161-142-156-53.ngrok-free.app";
+const API_URL = "http://4c5b-122-0-31-67.ngrok-free.app";
